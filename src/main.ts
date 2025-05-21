c85cf0ce1ea66ad46ed378cf551661f8207455cf
kfoq7 <frbrizoi@gmail.com>
2025-05-21 10:01:18 -0500

    chore: added pipes validatino transform

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './modules/core/app.module'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   const PORT = process.env.PORT ?? 8000
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  )
 
   await app.listen(PORT)
   console.log(`\n\n Server running on http://localhost:${PORT}\n\n`)
