fb3c19dc026a29c3b8e4ee2def5abb1ac11247c4
GRE3E <84554352+GRE3E@users.noreply.github.com>
2025-05-23 22:59:56 -0500

    fix anuncios

--- src/modules/announcement/announcement.module.ts
+++ src/modules/announcement/announcement.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { AnnouncementService } from './announcement.service'
 import { AnnouncementController } from './announcement.controller'
 import { Announcement } from './entities/announcement.entity'
+import { UserModule } from '../user/user.module'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Announcement])],
+  imports: [TypeOrmModule.forFeature([Announcement]), UserModule],
   controllers: [AnnouncementController],
   providers: [AnnouncementService],
   exports: [AnnouncementService],
